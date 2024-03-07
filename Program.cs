2a66ad1b0b7aab07a1a4b0ecb5b4a263656c1667
Gabriel-Viernes <viernesgabriel04@gmail.com>
2024-03-07 21:52:12 +0000

    main menu now functional when running program

--- Program.cs
+++ Program.cs
@@ -3,19 +3,55 @@ using System;
 using System.Collections.Generic;
 
 namespace yugiohLocalDatabase {
-    class mainMenu {
+    class Entry {
         static void Main(string[] args) {
+            Console.Clear();
+            bool quit = false;
             string[] mainMenuOptions = {"Add Card", "Find Card", "Change Card", "Delete Card", "Exit"};
+            Menu mainMenu = new Menu(mainMenuOptions);
+            mainMenu.Display();
+            while (quit == false) {
+                switch(Console.ReadKey().Key) {
+                    case System.ConsoleKey.UpArrow:
+                        if(mainMenu.index > 0) {
+                            mainMenu.index--;
+                        }
+                        Console.Clear();
+                        mainMenu.Display();
+                        break;
+                    case System.ConsoleKey.DownArrow:
+                        if(mainMenu.index < mainMenu.menuItems.Length-1) {
+                            mainMenu.index++;
+                        }
+                        Console.Clear();
+                        mainMenu.Display();
+                        break;
+                }
+            }
+        }
+    }
+
+    public class Menu {
+        public int index;
+        public string[] menuItems;
+        public Menu(string[] items) {
+            index = 0;
+            menuItems = items;
+        }
+        public void Display() {
             Console.WriteLine("======================================================");
             Console.WriteLine("         Welcome to the Yugioh Local Database!        ");
             Console.WriteLine("======================================================");
-            MenuDisplay(mainMenuOptions);
-        }
-        static void MenuDisplay(string[] menuItems) {
             for(int i = 0; i < menuItems.Length; i++) {
-                Console.WriteLine($"{menuItems[i]}");
+
+                if(i == index) {
+                    Console.WriteLine($"[[{menuItems[i]}]]");
+                } else {
+                    Console.WriteLine($"{menuItems[i]}");
+                }
             }
-        }
+            return;
+        }       
 
     }
 }
